a42318f86619da49fa00011b8929e4c66875bd85
marinedrp <marine.drp@outlook.com>
2023-02-02 15:21:13 +0100

    finished the style of the pages displayed after a successful payment or a payment that has been canceled

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -1,19 +1,69 @@
 import "./PaymentPage.css";
-import {Col, Container, Row } from "react-bootstrap";
+import ConfettiExplosion from "react-confetti-explosion";
+import Button from "react-bootstrap/esm/Button";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 function PaymentPage() {
   const path = window.location.pathname;
   console.log("this is the path:::", path);
-  const imageUrl = "https://img.freepik.com/free-vector/nature-background-design-with-golden-foil_52683-46174.jpg?w=2000"
+
+  const navigate = useNavigate();
+
+  const setConfettiExplosion = () => {
+    setTimeout(() => {
+        navigate("/profile");
+      }, 6500);
+  }
+
+  useEffect(() => {
+    if (path === "/payment/success"){
+        setConfettiExplosion()    
+    }
+  }, [])
+  
+  const explosion = {
+    force: 0.6,
+    duration: 5000,
+    particleCount: 200,
+    height: 1600,
+    width: 1600,
+    colors: ["#d5e1df", "#e3eaa7", "#b5e7a0", "#86af49"],
+  };
 
   return (
-      <div className="styled-div">
-        <div className="text-div">
-            <h1>Thank you for your purchase!</h1>
-            <p className="paragraph">We're thrilled to have you as a part of our growing community of plant lovers! <br/>
-            Your order is on its way and we can't wait for you to watch your new plants grow and flourish.</p>
-        </div>
+    <div className="styled-div">
+      <div className="text-div">
+        {path === "/payment/success" ? (
+          <>
+            <ConfettiExplosion {...explosion} />
+            <h1 className="title-success">Thank you for your purchase!</h1>
+            <p className="paragraph1">
+              We're thrilled to have you as a part of our growing community of
+              plant lovers! Your order is on its way and we can't wait for you
+              to watch your new plants grow and flourish. Wishing you a
+              wonderful day filled with greenery and growth.
+            </p>
+            <p className="paragraph2">The Plantastic Team. 🌿</p>
+            <p className="text-secondary">You will be redirected.</p>
+          </>
+        ) : (
+          <>
+            <h1 className="text-danger">Your payment has been canceled.</h1>
+            <p className="paragraph1">
+              We noticed that your recent payment for your order with us has
+              been cancelled. If you need any assistance or have any questions,
+              please don't hesitate to reach out to us.
+            </p>
+            <p className="paragraph2">The Plantastic Team. 🌿</p>
+            <Link to={"/cart"}>
+              <Button variant="success">Go back</Button>
+            </Link>
+          </>
+        )}
       </div>
+    </div>
   );
 }
 
