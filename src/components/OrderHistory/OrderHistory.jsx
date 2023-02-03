2bafbec1a1018b3d4b0f1f016b30e6b73ed2870c
Laura Bährs <laura.baehrs@googlemail.com>
2023-02-03 01:57:01 +0100

    start on profile page

--- src/components/OrderHistory/OrderHistory.jsx
+++ src/components/OrderHistory/OrderHistory.jsx
@@ -8,6 +8,7 @@ function OrderHistory({ eachOrder }) {
     setShowProductsInOrder(!showProductsInOrder);
   };
   return (
+    <>
     <div key={eachOrder._id}>
       <p>{new Date(eachOrder.createdAt).toLocaleDateString()}</p>
       <p>Total Price: {currencyFormatter.format(eachOrder.totalPrice)}</p>
@@ -26,6 +27,54 @@ function OrderHistory({ eachOrder }) {
         </div>
       )}
     </div>
+    <MDBCard>
+              <MDBRipple
+              rippleColor="light"
+              rippleTag="div"
+              className="bg-image rounded hover-overlay"
+            >
+              <MDBCardImage
+                src={plant.imageURL}
+                fluid
+                className="w-100"
+                style={{
+                  borderTopLeftRadius: "15px",
+                  borderTopRightRadius: "15px",
+                }}
+              />
+            </MDBRipple>
+            <MDBCardBody>
+              <div className="d-flex justify-content-between">
+                <p className="small">
+                  <a href="#!" className="text-muted">
+                    Laptops
+                  </a>
+                </p>
+                <p className="small text-danger">
+                  <s>$1099</s>
+                </p>
+              </div>
+
+              <div className="d-flex justify-content-between mb-3">
+                <h5 className="mb-0">HP Notebook</h5>
+                <h5 className="text-dark mb-0">$999</h5>
+              </div>
+
+              <div class="d-flex justify-content-between mb-2">
+                <p class="text-muted mb-0">
+                  Available: <span class="fw-bold">6</span>
+                </p>
+                <div class="ms-auto text-warning">
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                  <MDBIcon fas icon="star" />
+                </div>
+              </div>
+            </MDBCardBody>
+              </MDBCard>
+              </>
   );
 }
 
